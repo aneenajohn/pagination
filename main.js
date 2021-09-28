6377df1dbecba0fc297f562db4963a2fc562ece3
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-09-28 15:45:10 +0530

    removed console statements
    Signed-off-by: aneenajohn <75213145+aneenajohn@users.noreply.github.com>

--- main.js
+++ main.js
@@ -16,11 +16,9 @@ function DisplayList(items, wrapper, rows_per_page, page) {
 
     for (let i = 0; i < paginatedItems.length; i++) {
         let item = "Item " + paginatedItems[i];
-
         let item_element = document.createElement('div');
         item_element.classList.add('item');
         item_element.innerText = item;
-
         wrapper.appendChild(item_element);
     }
 }
@@ -29,8 +27,7 @@ function SetupPagination(items, wrapper, rows_per_page) {
     wrapper.innerHTML = "";
 
     let page_count = Math.ceil(items.length / rows_per_page);
-    var pages = Math.round(items.length / rows_per_page);
-    console.log({ page_count, pages })
+
     for (let i = 1; i < page_count + 1; i++) {
         let btn = PaginationButton(i, items);
         wrapper.appendChild(btn);
@@ -40,20 +37,16 @@ function SetupPagination(items, wrapper, rows_per_page) {
 function PaginationButton(page, items) {
     let button = document.createElement('button');
     button.innerText = page;
-    console.log({ page, items });
+
     if (current_page === page) button.classList.add('active');
 
     button.addEventListener('click', function() {
         current_page = page;
         DisplayList(items, list_element, rows, current_page);
-
         let current_btn = document.querySelector('.pagenumbers button.active');
-        console.log("b4", current_btn);
         current_btn.classList.remove('active');
-        console.log("aft", current_btn);
         button.classList.add('active');
     });
-
     return button;
 }
 
